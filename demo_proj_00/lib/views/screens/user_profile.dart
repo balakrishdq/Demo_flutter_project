6c358da3f1c12d02400fa12ad0662d8f3b7c87c5
balakrishdq <dqsoft0601@gmail.com>
2022-08-08 22:10:56 +0530

    till user profile screen

--- demo_proj_00/lib/views/screens/user_profile.dart
+++ demo_proj_00/lib/views/screens/user_profile.dart
@@ -1,5 +1,9 @@
 import 'package:demo_proj_00/utils/app_Styles.dart';
+import 'package:demo_proj_00/utils/app_layout.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:lottie/lottie.dart';
 
 class UserProfile extends StatefulWidget {
   const UserProfile({Key? key}) : super(key: key);
@@ -13,7 +17,201 @@ class _UserProfileState extends State<UserProfile> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Styles.bgColor,
-      body: Container(),
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+            horizontal: AppLayout.getWidth(20),
+            vertical: AppLayout.getHeight(20)),
+        children: [
+          Gap(AppLayout.getHeight(40)),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: AppLayout.getHeight(86),
+                width: AppLayout.getHeight(86),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(10)),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/img_1.webp'),
+                  ),
+                ),
+              ),
+              Gap(AppLayout.getHeight(10)),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Book Trip',
+                    style: Styles.headLineStyle1,
+                  ),
+                  Gap(AppLayout.getHeight(2)),
+                  Text(
+                    'Chennai',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                  Gap(AppLayout.getHeight(8)),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: AppLayout.getHeight(3),
+                        vertical: AppLayout.getHeight(3)),
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.circular(AppLayout.getHeight(100)),
+                      color: Color(0xFFFEF4F3),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(3),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFF526799),
+                          ),
+                          child: Icon(
+                            FluentSystemIcons.ic_fluent_shield_filled,
+                            color: Colors.white,
+                            size: 15,
+                          ),
+                        ),
+                        Gap(AppLayout.getHeight(6)),
+                        Text(
+                          'Premium Status',
+                          style: TextStyle(
+                            color: Color(0xFF526799),
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Gap(AppLayout.getHeight(5)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Column(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      print('you are tapped');
+                    },
+                    child: Text(
+                      'Edit',
+                      style: Styles.textStyle.copyWith(
+                          color: Styles.primaryColor,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          Gap(AppLayout.getHeight(8)),
+          Divider(
+            color: Colors.grey.shade300,
+          ),
+          Stack(
+            children: [
+              Container(
+                height: AppLayout.getHeight(90),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Styles.primaryColor,
+                  borderRadius: BorderRadius.circular(
+                    AppLayout.getHeight(18),
+                  ),
+                ),
+              ),
+              Positioned(
+                right: -45,
+                top: -40,
+                child: Container(
+                  padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                  decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      shape: BoxShape.circle,
+                      border: Border.all(width: 18, color: Color(0xFF513073))),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                    horizontal: AppLayout.getHeight(25),
+                    vertical: AppLayout.getHeight(20)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      child: Icon(
+                        FluentSystemIcons.ic_fluent_lightbulb_filament_filled,
+                        color: Styles.primaryColor,
+                        size: 27,
+                      ),
+                      maxRadius: 25,
+                      backgroundColor: Colors.white,
+                    ),
+                    Gap(AppLayout.getHeight(12)),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "You've got a Award",
+                          style: Styles.headLineStyle2.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "You've completed 60 trips in a year",
+                          style: Styles.headLineStyle4.copyWith(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white.withOpacity(0.9),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Gap(AppLayout.getHeight(20)),
+          Text(
+            'Accumulated miles',
+            style: Styles.headLineStyle2,
+          ),
+          Column(
+            children: [
+              Gap(AppLayout.getHeight(15)),
+              Text(
+                '16780',
+                style: TextStyle(
+                  fontSize: 45,
+                  color: Styles.textColor,
+                  fontWeight: FontWeight.w600,
+                ),
+              )
+            ],
+          ),
+          Gap(AppLayout.getHeight(10)),
+          Center(
+            child: Lottie.asset('assets/images/trip.json'),
+          ),
+          Gap(AppLayout.getHeight(15)),
+          Center(
+            child: Text('How to get more miles?',
+                style: Styles.textStyle.copyWith(
+                  color: Styles.primaryColor,
+                  fontWeight: FontWeight.w500,
+                )),
+          )
+        ],
+      ),
     );
   }
 }
